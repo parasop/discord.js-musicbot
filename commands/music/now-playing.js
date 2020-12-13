f0f03803abaae1ab934317ba20d80a64de1d6c41
parasop <63485824+parasop@users.noreply.github.com>
2020-12-13 17:48:22 +0530

    Update now-playing.js

--- commands/music/now-playing.js
+++ commands/music/now-playing.js
@@ -1,23 +1,72 @@
-const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
 
 module.exports = {
-    name: "nowplaying",
-    aliases: ["np"],
-
+  name: "nowplaying",
+  aliases: ["np"],
   run: async (client, message, args) => {
-    const serverQueue = client.queue.get(message.guild.id);
-    if (!serverQueue) {
-      message.channel.send("There is nothing playing in this server.");
-      let song = serverQueue.songs[0];
-      let np = new MessageEmbed()
-        .setAuthor("😀NOW PLAYING😀")
-        .setThumbnail(song.img)
-        .setColor("RANDOM")
-        .addField("Name", song.title, true)
-        .addField("Duration", song.duration, true)
-        .addField("Requested by", song.req.tag, true)
-        .setFooter(`${message.guild} PLAYER`);
-      return message.channel.send(np);
-    }
+    const Channel = message.member.voice.channel;
+
+    if (!Channel) return message.channel.send("join voice channel after try COMMAND");
+
+    const Queue = await client.queue.get(message.guild.id);
+
+    if (!Queue)
+      return message.channel.send(
+        "Nothing Is Playing Right Now, Add Some Songs To Queue"
+      );
+
+    const Song = await Queue.Songs[0],
+      Total = Song.Duration,
+      Seconds = Song.Seconds,
+      Time = parseInt(Queue.Bot.dispatcher.streamTime + Queue.ExtraTime);
+
+    function FD(duration) {
+      let minutes = Math.floor(duration / 60);
+      let hours = "";
+      if (minutes > 59) {
+        hours = Math.floor(minutes / 60);
+        hours = hours >= 10 ? hours : "0" + hours;
+        minutes = minutes - hours * 60;
+        minutes = minutes >= 10 ? minutes : "0" + minutes;
+      }
+      duration = Math.floor(duration % 60);
+      duration = duration >= 10 ? duration : "0" + duration;
+      if (hours != "") {
+        return hours + ":" + minutes + ":" + duration;
+      }
+      return minutes + ":" + duration;
+    };
+
+    const Sec = Math.round(Time / 1000),
+      AllTime = (Seconds * 1000).toFixed(0);
+    const Remaining = await FD((Seconds - Sec).toFixed(0));
+    const Adder = await FD(Sec);
+    const Index = Math.round((Time / AllTime) * 20);
+    const Bar = "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬".split("");
+    let ShowBar;
+
+    if (Index >= 1 && Index <= 20) {
+      Bar.splice(Index, 0, "🔵");
+      ShowBar = Bar.join("");
+    } else {
+      ShowBar = "🔵▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬";
+    };
+
+    const Data = `Song - **[${Song.Title}](${Song.Link})**\nCreator - **[${
+      Song.Author
+    }](${Song.AuthorLink})**\nUpload - **${
+      Song.Upload
+    }**\nViews - **${Song.Views ||
+      0}**\nDuration - **${Total}**\nRemaining - **${Remaining}**\n\n`;
+
+    const Embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setImage(Song.Thumbnail)
+      .setTitle("Now Playing!")
+      .setDescription(Data + `${ShowBar}\n${Adder}/${Total}`)
+      .setFooter(`Added By ${Song.Owner}`)
+      .setTimestamp();
+
+    return message.channel.send(Embed);
   }
 };
