6faa50d2e9e6e0e9c1641f67ecbe6932f3078d0c
parasop <63485824+parasop@users.noreply.github.com>
2020-12-14 14:30:28 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,164 +1,146 @@
 const { Util, MessageEmbed } = require("discord.js");
-//const { GOOGLE_API_KEY } = require('../../config');
-const YouTube = require("simple-youtube-api");
-const youtube = new YouTube(process.env.API);
 const ytdl = require("ytdl-core");
+const yts = require("yt-search");
 
 module.exports = {
   name: "play",
+  description: "To play songs :D",
+  usage: "<song_name>",
   aliases: ["p"],
-  run: async (client, message, args) => {
-    if (!args[0]) return message.channel.send("PROVIDE US SONG NAME OR LINK ");
-    args = message.content.split(" ");
-    const searchString = args.slice(1).join(" ");
-    const url = args[1] ? args[1].replace(/<(.+)>/g, "$1") : "";
 
-    const { channel } = message.member.voice;
-    if (!channel)
-      return message.channel.send(
-        "MUST JOIN VOICE CHANNEL BEFORE USING THIS COMMAND"
-      );
-
-    const permissions = channel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT")) {
-      message.channel.send(" I DON'T HAVE PERMISSION TO CONNECT YOUR VC");
+  run: async function(client, message, args) {
+    const channel = message.member.voice.channel;
+    if (!channel) {
+      message.channel.send("I am sorry but you need to be in a voice channel before using this commamd");
     }
-    if (!permissions.has("SPEAK")) {
-      message.channel.send(
-        "I DON'T HAVE PERMISSION TO PLAY SONG IN YOUR VOICE CHANNEL"
-      );
-    }
-    if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-      const playlist = await youtube.getPlaylist(url);
-      const videos = await playlist.getVideos();
 
-      for (const video of Object.values(videos)) {
-        const video2 = await youtube.getVideoByID(video.id);
-        await handleVideo(video2, message, channel, true);
-      }
-      return message.channel.send(
-        `**Playlist \`${playlist.title}\` has been added to the queue!**`
-      );
-    } else {
-      try {
-        var video = await youtube.getVideo(url);
-      } catch (error) {
-        try {
-          var videos = await youtube.searchVideos(searchString, 1);
-          var video = await youtube.getVideoByID(videos[0].id);
-        } catch (err) {
-          console.error(err);
-          return message.channel.send("CANNOT FIND TRY AGAIN");
+    if (!message.guild.me.hasPermission("CONNECT")) {
+      message.channel.send({
+        embed: {
+          color: "FF0000",
+          description:
+            "<:emoji_17:763367241327706118> I don't have permission to connect your vc!"
         }
-      }
-      return handleVideo(video, message, channel);
+      });
     }
-    async function handleVideo(video, message, channel, playlist = false) {
-      const serverQueue = client.queue.get(message.guild.id);
-      const songInfo = await ytdl.getInfo(video.id);
-      const song = {
-        id: video.id,
-        title: Util.escapeMarkdown(video.title),
-        url: `https://www.youtube.com/watch?v=${video.id}`,
-        thumbnail: `https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`,
-        duration: video.duration,
-        time: songInfo.videoDetails.lengthSeconds
-      };
-
-      if (serverQueue) {
-        serverQueue.songs.push(song);
-        if (playlist) return undefined;
-        else {
-          const sembed = new MessageEmbed()
-            .setColor("GREEN")
-            .setTitle("Added To Queue")
-            .setImage(song.thumbnail)
-            .setTimestamp()
-            .setDescription(
-              `
-   SONG NAME                         **${song.title}** 
-   
-Requested By **${message.author.username}**`
-            )
-            .setFooter(
-              message.member.displayName,
-              message.author.displayAvatarURL()
-            );
-          message.channel.send(sembed);
+    if (!message.guild.me.hasPermission("SPEAK")) {
+      message.channel.send({
+        embed: {
+          color: "FF0000",
+          description:
+            "<:emoji_17:763367241327706118>I need speak permission for playing music!"
         }
-        return undefined;
-      }
-
-      const queueConstruct = {
-        textChannel: message.channel,
-        voiceChannel: channel,
-        connection: null,
-        songs: [],
-        volume: 2.5,
-        playing: true,
-        loop: false
-      };
-      client.queue.set(message.guild.id, queueConstruct);
-      queueConstruct.songs.push(song);
-      try {
-        const connection = await channel.join();
-        queueConstruct.connection = connection;
-        play(queueConstruct.songs[0]);
-      } catch (error) {
-        console.error(`I could not join the voice channel: ${error.message}`);
-        client.queue.delete(message.guild.id);
-        await channel.leave();
-        return message.channel.send(
-          `I could not join the voice channel: ${error.message}`
-        );
-      }
+      });
     }
-    async function play(song) {
-      const queue = client.queue.get(message.guild.id);
+    var searchString = args.join(" ");
+    if (!searchString) {
+      message.channel.send("<:emoji_17:763367241327706118>provide us song' name or song's link");
+    }
+
+    var serverQueue = message.client.queue.get(message.guild.id);
+
+    var searched = await yts.search(searchString);
+    if (searched.videos.length === 0) {
+      message.channel.send("I can't find that song");
+    }
+    var songInfo = searched.videos[0];
+
+    const song = {
+      id: songInfo.videoId,
+      title: Util.escapeMarkdown(songInfo.title),
+      views: String(songInfo.views).padStart(10, " "),
+      url: songInfo.url,
+      ago: songInfo.ago,
+      duration: songInfo.duration.toString(),
+      img: songInfo.image,
+      req: message.author
+    };
+
+    if (serverQueue) {
+      serverQueue.songs.push(song);
+      let thing = new MessageEmbed()
+        .setTitle("SONG HAS BEEN ADDED TO QUEUE")
+        .setImage(song.img)
+        .setColor("ORANGE")
+        .setDescription(
+          `**SONG NAME**   
+[${song.title}](${song.url})     
+
+**DURACTION**
+${song.duration}
+
+**REQUEST BY**
+[${message.author}]
+
+
+        
+        
+        `
+        )
+        .setFooter(`PARAS GAMING 🇮🇳`);
+      return message.channel.send(thing);
+    }
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: channel,
+      connection: null,
+      songs: [],
+      volume: 3.5,
+      playing: true
+    };
+    message.client.queue.set(message.guild.id, queueConstruct);
+    queueConstruct.songs.push(song);
+
+    const play = async song => {
+      const queue = message.client.queue.get(message.guild.id);
       if (!song) {
-        queue.voiceChannel.leave();
-        //remove line for 24/7  vc
-        client.queue.delete(message.guild.id);
+         message.client.queue.delete(message.guild.id);
         return;
       }
 
-      let npmin = Math.floor(song.time / 60);
-      let npsec = song.time - npmin * 60;
-      let np = `${npmin}:${npsec}`.split(" ");
-
       const dispatcher = queue.connection
-        .play(
-          ytdl(song.url, { highWaterMark: 1 << 20, quality: "highestaudio" })
-        )
+        .play(ytdl(song.url))
         .on("finish", () => {
-          if (queue.loop) {
-            queue.songs.push(queue.songs.shift());
-            return play(queue.songs[0]);
-          }
           queue.songs.shift();
           play(queue.songs[0]);
         })
         .on("error", error => console.error(error));
       dispatcher.setVolumeLogarithmic(queue.volume / 5);
-      const embed = new MessageEmbed()
-        .setColor("GREEN")
-        .setTitle("Now Playing")
-        .setImage(song.thumbnail)
-        .setTimestamp()
+      let thing = new MessageEmbed()
+        .setTitle("START PLAYING")
         .setDescription(
           `
-Now playing:
-**${song.title}** 
-Song Length: 
-**${np}** `
-        )
-        .setFooter(
-          `Requested by: ${message.member.displayName}`,
-          message.author.displayAvatarURL()
-        );
-      queue.textChannel.send(embed);
+**SONG NAME**   
+[${song.title}](${song.url})     
 
-      //let's test our bot
+**DURACTION**
+${song.duration}
+
+**REQUEST BY**
+[${message.author}]
+`
+        )
+
+        .setImage(song.img)
+        .setColor("GREEN")
+        .setFooter(`PARAS GAMING🇮🇳`);
+      queue.textChannel.send(thing);
+    };
+
+    try {
+      const connection = await channel.join();
+      queueConstruct.connection = connection;
+      channel.guild.voice.setSelfDeaf(true);
+      play(queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`I could not join the voice channel: ${error}`);
+      message.client.queue.delete(message.guild.id);
+      //await channel.leave();
+      return console.log(
+        `I could not join the voice channel: ${error}`,
+        message.channel
+      );
     }
   }
 };
